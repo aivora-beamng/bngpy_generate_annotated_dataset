95627a635753bfb49855a92abde88131d72bcc28
Adam Ivora <aivora@beamng.gmbh>
2022-11-07 13:27:27 +0100

    Add annotation support for Python<3.10

--- generate_annotated_dataset.py
+++ generate_annotated_dataset.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 from itertools import cycle
 from pathlib import Path
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from itertools import cycle
 
 from beamngpy import BeamNGpy, Vehicle
